1ceae159d6b5cccbf141445b804ac0c630267bda
sangram chopdar <sangramdadasochopdar@gmail.com>
2024-03-08 19:15:01 +0530

    crud operations done

---
+++ crudeoperation/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import User from "./Components/User";
+import CreateUser from "./Components/CreateUser";
+import UpdateUser from "./Components/UpdateUser";
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<User />}></Route>
+          <Route path="/create" element={<CreateUser />}></Route>
+          <Route path="/update/:id" element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
+
+//
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//connect the database
+mongoose.connect("mongodb://localhost:27017/crudapp");
+
+//methods
+
+//to get all users
+app.get("/", (req, res) => {
+  UserModel.find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+//post
+app.post("/createUser", (req, res) => {
+  UserModel.create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+//get user to update
+
+app.get("/getUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findById({ _id: id })
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+//put after update
+app.put("/updateUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndUpdate(
+    { _id: id },
+    { name: req.body.name, email: req.body.email, age: req.body.age }
+  )
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+// delete user
+app.delete("/deleteUser/:id", (req, res) => {
+  const id = req.params.id;
+  UserModel.findByIdAndDelete({ _id: id })
+    .then((res) => res.json(res))
+    .catch((err) => res.json(err));
+});
+
+//
+app.listen(3001, () => {
+  console.log("server is running on port 3001");
+});
